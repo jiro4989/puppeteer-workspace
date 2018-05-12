f5318be569efe7d8ecbc85a1ccb988bebc555690
jiro4989 <jiroron666@gmail.com>
2018-05-12 17:33:56 +0900

    サンプルコード加筆。ただし正常動作していない...

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require("fs");
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -7,8 +8,25 @@ const puppeteer = require('puppeteer');
       '--disable-setuid-sandbox'
     ]
   });
+
+  const url = "https://jiro4989.github.io/";
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  // await page.goto('https://example.com');
+  await page.goto(url);
+  const list = page.evaluate(() => {
+    const nodes = document.querySelectorAll(".article-title");
+    const list = [];
+    for (let node in nodes) {
+      const text = node.innerHTML;
+      list.push(text);
+    }
+    return list;
+  });
+  let str = "";
+  for (let l in list) {
+    str += l;
+  }
+  fs.writeFile("result.txt", str);
   await page.screenshot({ path: 'example.png' });
 
   browser.close();
