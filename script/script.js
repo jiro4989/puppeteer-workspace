bd76008ff36b5a652e701d831204ded22ddb0151
jiro4989 <jiroron666@gmail.com>
2018-05-19 21:39:26 +0900

    DB情報を環境変数から取得するように修正

--- script/script.js
+++ script/script.js
@@ -49,10 +49,11 @@ const fs = require("fs");
   // ここからはDBにデータを追加する。
   const mysql = require("mysql");
   const conn = mysql.createConnection({
-    host: "pup_db",
-    user: "root",
-    password: "password",
-    database: "test_db"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
   });
 
   conn.connect((err) => {
